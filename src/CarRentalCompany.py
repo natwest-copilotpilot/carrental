d39b0f0962fc7e7666b194f47ba44e0e24bcf91e
Rajat Ratewal <rajat.ratewal@natwest.com>
2023-09-11 15:15:57 +0100

    Added Threading for matching cars

--- src/CarRentalCompany.py
+++ src/CarRentalCompany.py
@@ -1,17 +1,23 @@
+import threading
+
 class CarRentalCompany:
     def __init__(self):
         self.cars = []
+        self.lock = threading.Lock()
 
     def add_car(self, car):
         self.cars.append(car)
 
     def matching_cars(self, criteria):
         if criteria.get_cost_criteria() is not None:
-            cars_available = []
+            with self.lock:
+              return [car for car in self.cars if car.cost_per_day <= criteria.get_cost_criteria()]
+            """cars_available = []
             for car in self.cars:
                  if car.cost_per_day <= criteria.get_cost_criteria():
                      cars_available.append(car)
             return cars_available
+            """
 
     def rent_car(self, renter, car):
         pass
